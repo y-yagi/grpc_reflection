c4f3cf0ffb382f21a9c850aa372dcc9b0aafbb14
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-04-22 07:36:42 +0900

    bundle gem grpc_reflection

---
+++ lib/grpc_reflection.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "grpc_reflection/version"
+
+module GrpcReflection
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_grpc_reflection.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestGrpcReflection < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::GrpcReflection::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
