f3ad456c26c4aaca833b94f83accc20d832ed59a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-04-29 07:24:48 +0900

    Add the service for server reflection

--- lib/grpc_reflection.rb
+++ lib/grpc_reflection.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "grpc_reflection/version"
+require_relative "grpc_reflection/reflection/v1/reflection_services_pb"
 
 module GrpcReflection
   class Error < StandardError; end
