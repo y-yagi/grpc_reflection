bd1221b3a90d61b65c19fb2875978e5d1e003f65
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-04-29 07:40:06 +0900

    Implement list services response

---
+++ lib/grpc_reflection/server.rb
@@ -0,0 +1,27 @@
+require "grpc"
+require_relative "reflection/v1/reflection_services_pb"
+
+class GrpcReflection::Server < Grpc::Reflection::V1::ServerReflection::Service
+  def server_reflection_info(req, _unused_call)
+    request = req.first
+
+    res = Grpc::Reflection::V1::ServerReflectionResponse.new
+    # TODO: implement another responses
+    if request.list_services == "*"
+      res.list_services_response = Grpc::Reflection::V1::ListServiceResponse.new(service: list_services_response)
+    end
+    [res].enum_for(:each)
+  end
+
+  private
+
+  def service_names
+    @service_names ||= ObjectSpace.each_object(Class).filter_map {|k| k if k.ancestors.include?(GRPC::GenericService) }.map do |proto_servie|
+      proto_servie.service_name
+    end.uniq
+  end
+
+  def list_services_response
+    @list_services_response ||= service_names.map {|name| Grpc::Reflection::V1::ServiceResponse.new(name: name) }
+  end
+end
