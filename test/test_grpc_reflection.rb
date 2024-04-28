5c5c30e19662f8d3024d1bc07473064f506a3b9e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-04-29 07:50:14 +0900

    Remove unused tests

--- test/test_grpc_reflection.rb
+++ test/test_grpc_reflection.rb
@@ -3,11 +3,4 @@
 require "test_helper"
 
 class TestGrpcReflection < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::GrpcReflection::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
