b08393107587842d2fc9d8561c53be34b5ed5b8d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-08-18 12:50:03 +0900

    Load `deubg` by default

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "debug"
 require "grpc_reflection"
 require "minitest/hooks/test"
 require "minitest/autorun"
