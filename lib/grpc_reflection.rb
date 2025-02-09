0b991206f0aeb415b9c2fd9bc10abfefae5cd0c4
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2025-02-09 14:27:02 +0900

    wip

--- lib/grpc_reflection.rb
+++ lib/grpc_reflection.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+TracePoint.new(:c_call) do |tp|
+  puts "hi" if "#{tp.defined_class}.#{tp.method_id}" == "Google::Protobuf::DescriptorPool.add_serialized_file"
+end.enable
+
 require_relative "grpc_reflection/version"
 require_relative "grpc_reflection/file_descriptor_decorator"
 require_relative "grpc_reflection/file_descriptor_manager"
